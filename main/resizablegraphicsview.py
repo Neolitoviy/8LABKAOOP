2b3d256dbddf16deb0102bcc3b8c96508949b749
Neolitoviy <verukacolt@gmail.com>
2023-06-02 17:40:37 +0300

    +exe

--- main/resizablegraphicsview.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QGraphicsView
-from PyQt5.QtCore import Qt
-
-class ResizableGraphicsView(QGraphicsView):
-    def resizeEvent(self, event):
-        self.fitInView(self.sceneRect(), Qt.KeepAspectRatio)
-        super().resizeEvent(event)
